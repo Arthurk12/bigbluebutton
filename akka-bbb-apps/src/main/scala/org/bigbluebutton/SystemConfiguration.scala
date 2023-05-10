1cb8498f7beb11d3479d58668ae820f118e149aa
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2023-05-10 14:26:22 -0300

    fix(core): dialInEnforceGuestPolicy option in akka-apps
    Make enforcement of guest policy over dial-in/external endpoints optional in akka-apps
    
    Default is preserved (true)

--- akka-bbb-apps/src/main/scala/org/bigbluebutton/SystemConfiguration.scala
+++ akka-bbb-apps/src/main/scala/org/bigbluebutton/SystemConfiguration.scala
@@ -45,6 +45,7 @@ trait SystemConfiguration {
   lazy val syncVoiceUsersStatusInterval = Try(config.getInt("voiceConf.syncUserStatusInterval")).getOrElse(43)
   lazy val ejectRogueVoiceUsers = Try(config.getBoolean("voiceConf.ejectRogueVoiceUsers")).getOrElse(true)
   lazy val dialInApprovalAudioPath = Try(config.getString("voiceConf.dialInApprovalAudioPath")).getOrElse("ivr/ivr-please_hold_while_party_contacted.wav")
+  lazy val dialInEnforceGuestPolicy = Try(config.getBoolean("voiceConf.dialInEnforceGuestPolicy")).getOrElse(true)
   lazy val toggleListenOnlyAfterMuteTimer = Try(config.getInt("voiceConf.toggleListenOnlyAfterMuteTimer")).getOrElse(4)
 
   lazy val recordingChapterBreakLengthInMinutes = Try(config.getInt("recording.chapterBreakLengthInMinutes")).getOrElse(0)
