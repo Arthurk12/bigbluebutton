950b54267d5dbe9b8ebfab750e6edb8ccebce1c4
Arthurk12 <arthur_bockmann@hotmail.com>
2023-07-25 14:18:27 -0300

    fix(bbb-web): enhanced recordings recovery
    - Bypass meetings with no recording events
    - Improve verification of recorded meetings
    
    The script bypasses meetings with no recording events and verifies recorded
    meetings more effectively. Meetings that have been created with
    'record=true' and that have a 'RecordStatusEvent' in the stored events will
    have its done tags created.
    This script should be run before the bbb-web service is started.
    
    Co-authored-by: fcecagno <fcecagno@gmail.com>

--- bigbluebutton-web/pre-start.sh
+++ bigbluebutton-web/pre-start.sh
@@ -8,18 +8,31 @@ fi
 STATUS_DIR=/var/bigbluebutton/recording/status
 RECORDED_DIR=$STATUS_DIR/recorded
 ENDED_DIR=$STATUS_DIR/ended
+RECORD_STATUS_EVENT="RecordStatusEvent"
 
 isRecorded() {
-  if [ "$(echo $@ | grep "record true")" != "" ]; then
-    return 0
-  else
-    return -1
+  meetingId=$1
+  key=$2
+  recordingEventsNumber=$3
+
+  recorded=-1
+  if [ "$(redis-cli hget ${key} record)" == "true" ]; then
+    echo "Session has been created with record=true"
+    for eventId in $(redis-cli lrange meeting:${meetingId}:recordings 0 ${recordingEventsNumber} | cut -d'"' -f2); do
+      eventName=$(redis-cli hget recording:${meetingId}:${eventId} eventName)
+      if [ "${eventName}" == $RECORD_STATUS_EVENT ]; then
+        echo "Found RecordStatusEvent for meeting ${meetingId}"
+        recorded=0
+        break
+      fi
+    done
   fi
+  return $recorded
 }
 
-hasRecordedTag() {
+hasTag() {
   for file in "$RECORDED_DIR"/*; do
-    if [ "$(echo $file | grep $1)" != "" ]; then
+    if [ "$(echo $file | grep ${1})" != "" ]; then
       return 0
     fi
   done
@@ -28,7 +41,7 @@ hasRecordedTag() {
 
 hasEndedTag() {
   for file in "$ENDED_DIR"/*; do
-    if [ "$(echo $file | grep $1)" != "" ]; then
+    if [ "$(echo $file | grep ${1})" != "" ]; then
       return 0
     fi
   done
@@ -37,18 +50,36 @@ hasEndedTag() {
 
 echo "Collecting redis meeting info"
 KEYS=$(redis-cli keys "meeting:info:*")
+echo -e "Meetings found in redis are:\n${KEYS}"
 
 for key in $KEYS; do
   meetingId=$(echo $key | cut -d ':' -f 3)
-  info=$(redis-cli hgetall $key)
-  isRecorded $info; if [ $? == 0 ]; then
-    hasRecordedTag $meetingId; if [ $? != 0 ]; then
-      echo "Creating recorded done tag for meeting $meetingId"
-      touch $RECORDED_DIR/$meetingId".done"
+
+  echo "--------Processing meeting ${meetingId}--------"
+
+  recordingEventsNumber=$(redis-cli llen meeting:${meetingId}:recordings | cut -d' ' -f2)
+  if [ "${recordingEventsNumber}" == "0" ]; then
+    echo "No events stored for ${meetingId}"
+    echo "-----------------------------------------------------------------------------------------"
+    continue
+  fi
+
+  isRecorded $meetingId $key $recordingEventsNumber; if [ $? == 0 ]; then
+    echo "The meeting ${meetingId} is recorded"
+    hasTag $meetingId; if [ $? != 0 ]; then
+      echo "Creating recorded done tag for meeting ${meetingId}"
+      sudo -u bigbluebutton touch ${RECORDED_DIR}/${meetingId}.done
+    fi
+  else
+    hasTag $meetingId; if [ $? != 0 ]; then
+      echo "Creating non-recorded tag for meeting ${meetingId}"
+      sudo -u bigbluebutton touch ${RECORDED_DIR}/${meetingId}.norecord
     fi
   fi
+
   hasEndedTag $meetingId; if [ $? != 0 ]; then
-    echo "Creating ended done tag for meeting $meetingId"
-    touch $ENDED_DIR/$meetingId".done"
+    echo "Creating ended done tag for meeting ${meetingId}"
+    sudo -u bigbluebutton touch ${ENDED_DIR}/${meetingId}.done
   fi
+  echo "-----------------------------------------------------------------------------------------"
 done
