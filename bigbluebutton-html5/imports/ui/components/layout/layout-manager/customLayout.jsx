082ac0b4e12476b7f774e3e4ba9c2164874bac37
Arthurk12 <arthur_bockmann@hotmail.com>
2024-11-21 10:10:18 -0300

    refactor(layout-context): enhance widgets handling logic
    Refactors the layout context widgets handling code so that plugins can
    just be pulgged into this mechanism and to handle correctly the
    localization of widgets.

--- bigbluebutton-html5/imports/ui/components/layout/layout-manager/customLayout.jsx
+++ bigbluebutton-html5/imports/ui/components/layout/layout-manager/customLayout.jsx
@@ -142,11 +142,14 @@ const CustomLayout = (props) => {
             externalVideo, genericMainContent, screenShare, sharedNotes,
           } = prevInput;
           const { sidebarContentPanel } = sidebarContent;
+          const { registeredWidgets, pinnedWidgets } = sidebarNavigation;
           return defaultsDeep(
             {
               sidebarNavigation: {
                 isOpen:
                   sidebarNavigation.isOpen || sidebarContentPanel !== PANELS.NONE || false,
+                registeredWidgets,
+                pinnedWidgets,
               },
               sidebarContent: {
                 isOpen: sidebarContentPanel !== PANELS.NONE,
@@ -193,11 +196,14 @@ const CustomLayout = (props) => {
             externalVideo, genericMainContent, screenShare, sharedNotes,
           } = prevInput;
           const { sidebarContentPanel } = sidebarContent;
+          const { registeredWidgets, pinnedWidgets } = sidebarNavigation;
           return defaultsDeep(
             {
               sidebarNavigation: {
                 isOpen:
                   sidebarNavigation.isOpen || sidebarContentPanel !== PANELS.NONE || false,
+                registeredWidgets,
+                pinnedWidgets,
               },
               sidebarContent: {
                 isOpen: sidebarContentPanel !== PANELS.NONE,
