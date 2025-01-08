fc97aed4ecaccc82770e03d1a10f42b15c460587
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2025-01-08 20:40:49 +0000

    fix(audio): client state not fully in sync when using LiveKit audio
    The client state (i.e.: audio-manager's isConnected prop) might be
    out-of-sync compared to the server state when using LiveKit. Sample
    scenario: autoJoin=false or closing the audio modal.
    Not joining audio is not an option with LiveKit as it happens automatically,
    but audio-manager still presumes it is. This means the audio join callback
    is only called when join is explictly initiated by one of it's joinAudio
    calls, which causes the state desync.
    
    Add tracking for the server audio state when using LiveKit and guarantee
    audio-manager is in sync. This is done in a way that does not interfere
    with other audio bridges.

--- bigbluebutton-html5/imports/ui/components/livekit/component.tsx
+++ bigbluebutton-html5/imports/ui/components/livekit/component.tsx
@@ -8,6 +8,7 @@ import {
   useConnectionState,
 } from '@livekit/components-react';
 import {
+  ConnectionState,
   type Room,
   type RoomConnectOptions,
 } from 'livekit-client';
@@ -50,9 +51,16 @@ const LiveKitObserver = ({
   const [setUserTalking] = useMutation(USER_SET_TALKING);
   const isSpeaking = useIsSpeaking(localParticipant);
   const connectionState = useConnectionState(room);
+  const { data: currentUserData } = useCurrentUser((u) => ({
+    voice: {
+      joined: u.voice?.joined ?? false,
+    },
+  }));
   /* eslint no-underscore-dangle: 0 */
   // @ts-ignore
   const isMuted = useReactiveVar(AudioManager._isMuted.value) as boolean;
+  // @ts-ignore
+  const isAudioManagerConnected = useReactiveVar(AudioManager._isConnected.value) as boolean;
 
   useEffect(() => {
     logger.debug({
@@ -73,6 +81,19 @@ const LiveKitObserver = ({
       },
     });
   }, [isSpeaking, isMuted]);
+
+  useEffect(() => {
+    // If the user is connected to LiveKit and server-side audio state is present,
+    // but audio-manager is not connected, run the onAudioJoin callback to mark
+    // it as connected. Reasoning: there's no option not to connect to audio
+    // with LiveKit, so this ensures the client-side audio state is in sync with
+    // this automatic behavior.
+    if (!isAudioManagerConnected
+      && connectionState === ConnectionState.Connected
+      && currentUserData?.voice?.joined) {
+      AudioManager.onAudioJoin();
+    }
+  }, [isAudioManagerConnected, currentUserData, connectionState]);
 
   return null;
 };
@@ -155,7 +176,7 @@ const BBBLiveKitRoomContainer: React.FC = () => {
   const [meetingSettings] = useMeetingSettings();
   const url = meetingSettings.public.media?.livekit?.url
     || `wss://${window.location.hostname}/livekit`;
-  const withSelectiveSubscription = meetingSettings.public.media?.livekit?.selectiveSubscription || false;
+  const withSelectiveSubscription = meetingSettings.public.media?.livekit?.selectiveSubscription ?? false;
   const { data: bridges } = useMeeting((m) => ({
     cameraBridge: m.cameraBridge,
     screenShareBridge: m.screenShareBridge,
