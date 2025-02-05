03e10231d876240bed90d6dc1424cb1ed8e6f89c
Arthurk12 <arthur_bockmann@hotmail.com>
2025-02-05 16:27:20 -0300

    style: change border colors
    Changes border colors to new color defined by the design team.

--- bigbluebutton-html5/imports/ui/components/sidebar-content/styles.ts
+++ bigbluebutton-html5/imports/ui/components/sidebar-content/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import {
   colorWhite,
   colorPrimary,
-  colorGrayLightest,
+  colorBorder,
   appsPanelTextColor,
   colorBackground,
 } from '/imports/ui/stylesheets/styled-components/palette';
@@ -124,7 +124,7 @@ export const HeaderContainer = styled(Header)`
 export const Separator = styled.hr`
   width: 100%;
   border: 0;
-  border-bottom: 1px solid ${colorGrayLightest};
+  border-bottom: 1px solid ${colorBorder};
 `;
 
 export const PanelContent = styled.div`
