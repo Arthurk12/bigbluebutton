fb973410c4609134e2f29bc7933017f763681441
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2025-02-07 12:08:18 +0000

    feat(livekit): implement "Leave audio"
    Implement "Leave audio" for LiveKit through a newly added "deafen"
    functionality. Whenever an user "leaves" audio, they'll be deafened,
    which causes the selective subscription component to pause all
    subscriptions (i.e.: no sound).
    
    As far as UI is concerned, nothing should be different for now. It
    should react the same as leaving audio through mediasoup/FS, even if the
    internals work differently.
    
    This also fixes a couple of warts re. audio when returning to/joining
    into  breakout rooms while using LiveKit.
    
    Additionally:
      - Disable listen only mode (and all its related userdata/settings
        parameters) when using LiveKit.

--- bigbluebutton-html5/imports/ui/components/user-list/user-list-participants/list-item/service.ts
+++ bigbluebutton-html5/imports/ui/components/user-list/user-list-participants/list-item/service.ts
@@ -133,7 +133,7 @@ export const generateActionsPermissions = (
   isPrivateChatEnabled: boolean,
 ) => {
   const subjectUserVoice = subjectUser.voice;
-
+  const subjectUserInAudio = subjectUserVoice?.joined && !subjectUserVoice?.deafened;
   const amIModerator = currentUser.isModerator;
   const isDialInUser = isVoiceOnlyUser(subjectUser.userId);
   const amISubjectUser = isMe(subjectUser.userId);
@@ -157,14 +157,14 @@ export const generateActionsPermissions = (
     && !isBreakout;
 
   const allowedToMuteAudio = hasAuthority
-    && subjectUserVoice?.joined
+    && subjectUserInAudio
     && !isMuted
     && !subjectUserVoice?.listenOnly
     && !isSubjectUserBot
     && !isBreakout;
 
   const allowedToUnmuteAudio = hasAuthority
-    && subjectUserVoice?.joined
+    && subjectUserInAudio
     && !subjectUserVoice.listenOnly
     && isMuted
     && (amISubjectUser || usersPolicies?.allowModsToUnmuteUsers)
