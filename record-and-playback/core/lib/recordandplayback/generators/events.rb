cc00f7f55edfd49e048c91688500e83fe68441a4
germanocaumo <germanocaumo@gmail.com>
2025-02-25 13:50:41 -0300

    fix(recording): crash when duration_from_file is nil

--- record-and-playback/core/lib/recordandplayback/generators/events.rb
+++ record-and-playback/core/lib/recordandplayback/generators/events.rb
@@ -205,6 +205,9 @@ module BigBlueButton
             if /^medium-v_\d+-\d+\.\w+$$/.match File.basename(filename)
               BigBlueButton.logger.warn("This is most likely a bug of recording the SIP video twice, so it's going to be ignored")
               next
+            elsif duration_from_file.nil?
+              BigBlueButton.logger.warn("No file info information, ignoring Stop event without prior Start for video #{File.basename(filename)}")
+              next
             else
               event_filename = event.at_xpath('filename').text
               if ! events_copy.xpath("/recording/event[@module='bbb-webrtc-sfu' and @eventname='StartWebRTCShareEvent' and @timestamp>#{timestamp} and ./filename='#{event_filename}']").empty?
