73d2397d223ed54b79c86d544e6e221d80dc0e39
João Victor <joaovictornunes973@gmail.com>
2025-02-26 10:24:55 -0300

    fix(whiteboard): Clamp presentation width to a minimum value of 0 - When calculating zoom with gap, clamp the resulting presentation width to a minimum value of 0 when subtracting the gap value.

--- bigbluebutton-html5/imports/ui/components/whiteboard/component.jsx
+++ bigbluebutton-html5/imports/ui/components/whiteboard/component.jsx
@@ -1003,7 +1003,7 @@ const Whiteboard = React.memo((props) => {
     localHeight,
     widthAdjustment = 0,
   ) => {
-    const presentationWidthLocal = presentationAreaWidth - widthAdjustment;
+    const presentationWidthLocal = Math.max(presentationAreaWidth - widthAdjustment, 0);
     const calcedZoom = (fitToWidth
       ? presentationWidthLocal / localWidth
       : Math.min(
