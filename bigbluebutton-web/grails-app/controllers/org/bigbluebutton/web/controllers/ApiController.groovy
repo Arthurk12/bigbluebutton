1f0f88c8c9e3dff7e7c419b25a1816acc56c6404
prlanzarin <4529051+prlanzarin@users.noreply.github.com>
2025-05-13 12:54:43 +0000

    fix: remove breakout restriction for providing userdata via join
    Commit 49901b56 in our side removed both viewer and breakout
    restrictions for providing userdata via join calls.
    Upstream 1f0e27a20b1 refactored this to add a userdata ignore list and
    removed the viewer restriction, but kept the breakout one.
    
    We need both cases to work, so the restriction is being removed on our
    side first. This has to be propagated to upstream later if it makes
    sense.

--- bigbluebutton-web/grails-app/controllers/org/bigbluebutton/web/controllers/ApiController.groovy
+++ bigbluebutton-web/grails-app/controllers/org/bigbluebutton/web/controllers/ApiController.groovy
@@ -1252,24 +1252,22 @@ class ApiController {
         boolean isModerator = us.role?.equals(ROLE_MODERATOR);
         boolean blockAllUserdataForViewers = userdataBlocklistForViewers.any { it.equalsIgnoreCase("all") };
 
-        if (!meeting.isBreakout()) {
-          request.getParameterMap()
-                  .findAll { key, value ->
-                    // always allow `enforceLayout`
-                    if (key == "enforceLayout") return true
+        request.getParameterMap()
+          .findAll { key, value ->
+            // always allow `enforceLayout`
+            if (key == "enforceLayout") return true
 
-                    // For prefix userdata-
-                    if (key.startsWith("userdata-")) {
-                      if (isModerator) return true
-                      if (blockAllUserdataForViewers) return false
-                      return !userdataBlocklistForViewers.contains(key - "userdata-")
-                    }
+              // For prefix userdata-
+              if (key.startsWith("userdata-")) {
+                if (isModerator) return true
+                  if (blockAllUserdataForViewers) return false
+                    return !userdataBlocklistForViewers.contains(key - "userdata-")
+              }
 
-                    return false
-                  }
-                  .findAll { key, value -> !StringUtils.isEmpty(value[-1]) }
-                  .each { key, value -> queryParameters.put(key, value[-1]) }
-        }
+            return false
+          }
+        .findAll { key, value -> !StringUtils.isEmpty(value[-1]) }
+        .each { key, value -> queryParameters.put(key, value[-1]) }
 
         String httpQueryString = "";
         for(String parameterName : queryParameters.keySet()) {
